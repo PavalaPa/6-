07cd08377bccbbdadb0f377fa1f2a16d2e981714
PavalaPa <93196111+PavalaPa@users.noreply.github.com>
2023-12-23 18:04:01 +0300

    Update 6 лаба.py

--- 6 лаба.py
+++ 6 лаба.py
@@ -1,15 +1,35 @@
 import os
-print('Enter folder path')
-path = os.path.abspath(input())
-size = 0
-max_size = 0
-max_file = ''
-for folder, subfolder, files in os.walk(path):
-    for file in files:
-        size = os.stat(os.path.join(folder, file)).st_size
-        if size > max_size:
-            max_size = size
-            max_file = os.path.join(folder, file)
 
-print(f'The largest file is: {max_file}')
-print(f'Size:{str(max_size)} bytes')
+while True:
+    path = os.path.abspath(input('Enter folder path: '))
+    if os.path.exists(path):
+        break
+    print('File not found :(((((')
+
+output_file_path = os.path.join(path, 'hierarchy.txt')
+
+with open(output_file_path, 'w') as output_file:
+    for folder, subfolders, files in os.walk(path):
+        max_files = []
+        max_size = 0
+
+        for file in files:
+            file_path = os.path.join(folder, file)
+            size = os.stat(file_path).st_size
+
+            if size > max_size:
+                max_size = size
+                max_files = [(file, file_path, size)]
+            elif size == max_size:
+                max_files.append((file, file_path, size))
+
+        if max_files:
+            output_file.write(f'Folder: {folder}\n')
+            for file_info in max_files:
+                file_name, file_path, file_size = file_info
+                output_file.write(f'File: {file_name}\n')
+                output_file.write(f'Path: {file_path}\n')
+                output_file.write(f'Size: {file_size} bytes\n')
+            output_file.write('\n')
+
+print(f'Information written to {output_file_path}')
